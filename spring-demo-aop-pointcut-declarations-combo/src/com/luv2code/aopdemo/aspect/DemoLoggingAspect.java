ccdc85c101c09b955b3e638305e36d9d160df80d
Vlad <johnytynky@gmail.com>
2020-11-01 13:27:14 +0700

    AOP: Pointcut Declarations
    Section 38

---
+++ spring-demo-aop-pointcut-declarations-combo/src/com/luv2code/aopdemo/aspect/DemoLoggingAspect.java
@@ -0,0 +1,35 @@
+package com.luv2code.aopdemo.aspect;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class DemoLoggingAspect {
+
+	@Pointcut("execution(* com.luv2code.aopdemo.dao.*.*(..))")
+	private void forDAOPackage() {} 
+	
+	@Pointcut("execution(* com.luv2code.aopdemo.dao.*.get*(..))")
+	private void getter() {}
+
+	@Pointcut("execution(* com.luv2code.aopdemo.dao.*.set*(..))")
+	private void setter() {}
+
+	@Pointcut("forDAOPackage() && !(getter() || setter())")
+	private void forDAOPackageNoGetterSetter() {}
+	
+	@Before("forDAOPackageNoGetterSetter()")
+	public void beforeAddAccountAdvice() {
+		
+		System.out.println("\n=========>>>>>> Executing @Before advice on method");
+	}
+
+	@Before("forDAOPackageNoGetterSetter()")
+	public void performAPIAnalytics() {
+
+		System.out.println("=========>>>>>> Performing API Analytics");
+	}
+}
---
+++ spring-demo-aop-pointcut-declarations/src/com/luv2code/aopdemo/dao/AccountDAO.java
@@ -0,0 +1,21 @@
+package com.luv2code.aopdemo.dao;
+
+import org.springframework.stereotype.Component;
+
+import com.luv2code.aopdemo.Account;
+
+@Component
+public class AccountDAO {
+
+	public void addAccount(Account account, boolean flag) {
+
+		System.out.println(getClass() + " Doing DB work: adding an account");
+	}
+
+	public boolean doWork() {
+
+		System.out.println(getClass() + " doWork()");
+
+		return true;
+	}
+}
